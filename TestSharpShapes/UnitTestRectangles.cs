1c8e1d363ecb46f0cbc71e6e154c07a1883dca0d
brandontate <blt900@gmail.com>
2015-01-26 11:22:28 -0600

    Added 6 tests for Rectangles

---
+++ TestSharpShapes/UnitTestRectangles.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
+
+namespace TestSharpShapes
+{
+    [TestClass]
+    public class UnitTestRectangles
+    {
+        [TestMethod]
+        public void TestRectangleConstructor()
+        {
+            Rectangle rectangle = new Rectangle(40, 50);
+            Assert.AreEqual(40, rectangle.Width);
+            Assert.AreEqual(50, rectangle.Height);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleContructorSanityChecksWidth()
+        {
+            Rectangle rectangle = new Rectangle(0, 50);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleContructorSanityChecksHeight()
+        {
+            Rectangle rectangle = new Rectangle(50, 0);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleContructorSanityChecksHeightPositivity()
+        {
+            Rectangle rectangle = new Rectangle(50, -1);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleContructorSanityChecksWidthPositivity()
+        {
+            Rectangle rectangle = new Rectangle(-1, 50);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleContructorSanityRequiresValues()
+        {
+            Rectangle rectangle = new Rectangle();
+
+        }
+    }
+}
